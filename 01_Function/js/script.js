d1b3c1b46e80f5b1f2fe2bb6ce7f19cfa3d879ca
ANSILYAS <114913715+ansilyas@users.noreply.github.com>
2023-01-16 07:47:39 +0500

    Add files via upload

---
+++ 01_Function/js/script.js
@@ -0,0 +1,16 @@
+// Q1
+let a="Ans"
+let b=6
+console.log(a+b)
+
+// Q2
+const a1={
+    name:"Ans",
+    class:"BS(IT)",
+    Section:1,
+    isprincipal:false
+ }
+ a1['friend']="Ahmed"
+ a1['name']="ilyas"
+ console.log(a1)
+console.log(a1.class)
